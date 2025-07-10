23d8e89e310b37b0fd7186b084a4a6e6bed3852d
ElSantanax <josesantanaserrano130598@gmail.com>
2025-07-10 09:19:22 -0500

    IndexedDB

--- js/app.js
+++ js/app.js
@@ -1,34 +1,67 @@
-function crmDB() {
-    // Crear base de datos versión 1.0
-    let crmDB = window.indexedDB.open('crm', 1)
+let DB;
 
-    // Si hay error
+document.addEventListener('DOMContentLoaded', () => {
+    crmDB();
+
+    setTimeout(() => {
+        crearCliente();
+    }, 5000);
+});
+
+function crmDB() {
+    let crmDB = window.indexedDB.open('crm', 1);
+
     crmDB.onerror = function () {
         console.log('Error en la creación de la Base de Datos');
-    }
+    };
 
-    // Si se creo bien.
     crmDB.onsuccess = function () {
-        console.log('Bases de Datos creada correctamente');
-    }
+        console.log('Base de Datos creada correctamente');
+        DB = crmDB.result;
+    };
 
-    // Configuración de la base de datos
     crmDB.onupgradeneeded = function (e) {
-
         const db = e.target.result;
 
         const objectStore = db.createObjectStore('crm', {
-            keyPath: 'crm',
+            keyPath: 'id',
             autoIncrement: true
         });
 
-        // Definir las columnas
         objectStore.createIndex('nombre', 'nombre', { unique: false });
-        objectStore.createIndex('correo', 'correo', { unique: true });
+        objectStore.createIndex('email', 'email', { unique: true });
         objectStore.createIndex('telefono', 'telefono', { unique: false });
 
-        console.log('Columnas creada');
-    }
+        console.log('Columnas creadas');
+    };
 }
 
-crmDB();
+function crearCliente() {
+    const transaction = DB.transaction(['crm'], 'readwrite');
+
+    transaction.oncomplete = function () {
+        console.log('Transacción completada');
+    };
+
+    transaction.onerror = function () {
+        console.log('Hubo un error en la transacción');
+    };
+
+    const objectStore = transaction.objectStore('crm');
+
+    const nuevoCliente = {
+        telefono: 1920394,
+        nombre: 'José',
+        email: 'correo@correo.com'
+    };
+
+    const peticion = objectStore.add(nuevoCliente);
+
+    peticion.onsuccess = function () {
+        console.log('Cliente agregado correctamente');
+    };
+
+    peticion.onerror = function () {
+        console.log('Error al agregar el cliente');
+    };
+}
